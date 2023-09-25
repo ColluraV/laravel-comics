e5d5556dd5d607de339198c7429a764b701912cf
ColluraV <valerio.18.02.89@hotmail.it>
2023-09-25 14:54:20 +0200

    files importati da slack

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,46 @@
-@vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+<html lang="it">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laravel_Comics</title>
+        
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        
+                
+        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+
+</head>
+
+<body>
+
+    <main>
+
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-6">
+                    <h1>Benvenuto </h1>
+                    <div class="">
+ 
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+    </main>
+
+    <script src=""></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
