641463678704c0f3e4d54b884167a8cddb1d6bb2
ColluraV <valerio.18.02.89@hotmail.it>
2023-09-25 18:08:30 +0200

    ok

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -15,28 +15,28 @@
                     {{-- ////////////// cards in ciclo for //////////////// --}}
                     @foreach ($comicsList as $cardComics)
                         <div class="col">
-                           
+
                             <div class="card border-0 rounded-0 h-100">
-                                <img class="relative card-img-top" src={{$cardComics["thumb"]}} alt="" />{{-- //////immagine////// --}}
-                            
+                                <img class="relative card-img-top" src={{ $cardComics['thumb'] }}
+                                    alt="" />{{-- //////immagine////// --}}
+
                                 <div class="card-body text-white bg-dark"> {{-- //////nome////// --}}
-                                {{ $cardComics["series"] }}
-                                </div>
-                            
-                                <div class="overlay"> {{-- //////info in hover////// --}}
-                                    <div class="absolute top hidden">Tipologia: <br>{{ $cardComics["type"] }}</div>
-                                    <div class="absolute bottom hidden">Prezzo: {{ $cardComics["price"] }}</div>
+                                    {{ $cardComics['series'] }}
                                 </div>
 
-                                @endforeach
-                        </div>  
+                                <div class="overlay"> {{-- //////info in hover////// --}}
+                                    <div class="absolute top hidden">Tipologia: <br>{{ $cardComics['type'] }}</div>
+                                    <div class="absolute bottom hidden">Prezzo: {{ $cardComics['price'] }}</div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    @endforeach
 
                 </div>
-                
             </div>
-        </div>
-        
-        
-    </div>
 
-@endsection
+
+        </div>
+    @endsection
